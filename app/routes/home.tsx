f5de7e20060e54e3033c8c524d27667d53687401
thisisfaizanali <faizanaliwhb@gmail.com>
2025-11-12 15:01:14 +0530

    Implement Puter integration with Zustand store; add authentication and file system functionalities

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,9 @@
-import { resumes } from 'constants';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 import Navbar from '~/components/Navbar';
 import ResumeCard from '~/components/ResumeCard';
+import { usePuterStore } from '~/lib/puter';
+import { resumes } from '../../constants';
 import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
@@ -11,6 +14,13 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
